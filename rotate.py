b25014096a40fc8ca774feb366405b47e1370aae
acarrasquillo <alex.carrasquillo@lynaerospace.net>
2023-03-07 16:24:24 -0800

    Start multi-page pdf rotation logic (untested)

--- rotate.py
+++ rotate.py
@@ -29,6 +29,31 @@ def rotate(filepath: pathlib.Path, angle: int) -> None:
 
     return
 
+def rotateMultiPage(filepath : pathlib.Path, angle : int) -> None:
+    if not filepath.exists():
+        print(f'File not found: {filepath}', file=sys.stderr)
+        return
+
+    filepathOut = ('rotated_' + filepath.name)
+    if filepath.parent:
+        filepathOut = filepath.parent / filepathOut
+    print(f'Source file: {filepath.name}')
+    print(f'Rotated file will be saved as: {filepathOut.name}')
+
+    try:
+        with open(filepath, 'rb') as fin, open(filepathOut, 'wb') as fout:
+            reader = PyPDF2.PdfReader(fin)
+            writer = PyPDF2.PdfWriter(fout)
+
+            for page in reader.pages:
+                page.rotate(angle)
+                writer.add_page(page)
+
+    except PermissionError as e:
+        print(e, file = sys.stderr)
+
+    return
+
 
 def main(argv: list[str]):
     filepath = pathlib.Path.home() / 'Lyn Documents/Security/IS Training/Signed/IS_Training_2023.pdf'
