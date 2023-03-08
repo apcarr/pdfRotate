d32f0255b8dfcf19535f0c116c4e178b6c50b295
acarrasquillo <alex.carrasquillo@lynaerospace.net>
2023-03-07 16:08:34 -0800

    Rotate Hard Coded Single Page PDF

---
+++ rotate.py
@@ -0,0 +1,37 @@
+import os
+import pathlib
+import PyPDF2
+import sys
+
+def rotate(filepath : pathlib.Path, angle : int) -> None:
+    if not filepath.exists():
+        print(f'File not found: {filepath}', file=sys.stderr)
+        return
+
+    filepathOut = ('rotated_' + filepath.name)
+    if filepath.parent:
+        filepathOut = filepath.parent / filepathOut
+    print(f'Source file: {filepath.name}')
+    print(f'Rotated file will be saved as: {filepathOut.name}')
+    
+    try:
+        with open(filepath, 'rb') as fin, open(filepathOut, 'wb') as fout:
+            reader = PyPDF2.PdfReader(fin)
+            writer = PyPDF2.PdfWriter(fout)
+
+            page = reader.pages[-1]
+            page.rotate(angle)
+            writer.add_page(page)
+            writer.write(fout)
+
+    except (FileNotFoundError, PermissionError) as e:
+        print(e, file = sys.stderr)
+
+    return
+
+def main(argv : list[str]):
+    filepath = pathlib.Path.home() / 'Lyn Documents/Security/IS Training/Signed/IS_Training_2023.pdf'
+    rotate(filepath, 90)
+
+if __name__ == '__main__':
+    main(sys.argv)
